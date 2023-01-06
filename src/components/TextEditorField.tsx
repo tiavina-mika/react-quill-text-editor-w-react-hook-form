e2f034f0dd4d8828274e5522c3d6e20ba0f0ec6b
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-01-06 10:57:23 +0300

    add toolbar options

--- src/components/TextEditorField.tsx
+++ src/components/TextEditorField.tsx
@@ -6,6 +6,23 @@ import "react-quill/dist/quill.snow.css";
 
 import ReactQuill from "react-quill";
 
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }]
+];
+
 type Props = {
   name: string;
   label?: string;
@@ -28,7 +45,13 @@ const TextEditorField: FC<Props> = ({ name, label, helperText }) => {
           {label && (
             <InputLabel sx={{ mb: 0.8, color: "#000" }}>{label}</InputLabel>
           )}
-          <ReactQuill theme="snow" value={value} onChange={onChange} />;
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={onChange}
+            modules={{ toolbar: toolbarOptions }}
+          />
+          ;
           {errors[name] && (
             <FormHelperText error>{(errors as any)[name]}</FormHelperText>
           )}
